783fc43f6d9919966d3bff98a4256e661be9bde0
Randy Seltzer <rjs6502@gmail.com>
2018-06-19 11:44:51 -0400

    Sencha App Setup

---
+++ build/temp/development/FantasyUCI/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/randys/Documents/Randy/Personal_Docs/Projects/JS/FantasyUCI/app.js
---
+++ build/temp/production/FantasyUCI/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = '/Users/randys/Documents/Randy/Personal_Docs/Projects/JS/FantasyUCI/build/.sass-cache'
